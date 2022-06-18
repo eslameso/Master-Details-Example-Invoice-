c4c960f1d3b7fddbe001341820efa4196ca54c97
Eslam.Elsayed <engeso8@gmail.com>
2022-06-18 19:52:12 +0200

    Total Calc

--- InvoiceTest/wwwroot/js/Row.js
+++ InvoiceTest/wwwroot/js/Row.js
@@ -1,11 +1,11 @@
 ﻿//adding new row to the body
 const addRow = function (i) {
     var htmlElenent = `<tr>
-                    <td><input asp-for="@Model.Details[${i}].ItemName" class="form-control"/></td>
-                    <td><input asp-for="@Model.Details[${i}].Price" class="form-control" /></td>
-                    <td><input asp-for="@Model.Details[${i}].Quantity" class="form-control" /></td>
-                    <td><input asp-for="@Model.Details[${i}].Discount" class="form-control" /></td>
-                    <td><input asp-for="@Model.Details[${i}].Total" class="form-control" /></td>
+                    <td><input asp-for="@Model.Details[${i}].ItemName" class="form-control ItemName"/></td>
+                    <td><input type="number" asp-for="@Model.Details[${i}].Price" class="form-control Price" /></td>
+                    <td><input type="number" asp-for="@Model.Details[${i}].Quantity" class="form-control Quantity" /></td>
+                    <td><input type="number" asp-for="@Model.Details[${i}].Discount" class="form-control Discount" placeHolder="                           %" /></td>
+                    <td><input type="number" asp-for="@Model.Details[${i}].Total" class="form-control Total" disabled /></td>
                     <td>
                         <button type="button" class="btn btn-danger btn-circle btn-sm deleteBtn">X</button>
                     </td>
